5a7c8e6d4861b302f1a48d8a885815f67a3c2928
kaluzsikc <kaluzsikc@gmail.com>
2018-10-15 19:41:58 +0100

    Employee class created with getters for all properties and added raiseSalary and payBonus methods

---
+++ src/main/java/Employee/Employee.java
@@ -0,0 +1,35 @@
+package Employee;
+
+
+
+public class Employee {
+    private String name;
+    private int NINumber;
+    private double salary;
+
+    public Employee(String name, int NINumber, double salary){
+        this.name = name;
+        this.NINumber = NINumber;
+        this.salary = salary;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getNINumber(){
+        return this.NINumber;
+    }
+
+    public double getSalary(){
+        return this.salary;
+    }
+
+    public void raiseSalary(double salaryRaise){
+        this.salary += salaryRaise;
+    }
+
+    public double payBonus(){
+       return this.salary/100;
+    }
+}
